621306d710daa0a2bf83a833d02563a320da36f4
Marco Silva <marant_silva@hotmail.com>
2018-11-21 11:10:14 +0000

    desenvolvida a função build que fica responsável por construir um ficheiro que contem os links de todos os filmes presentes no site. Implementado também a possibilidade de construir ou não esse ficheiro colocando a opção '-b'

--- TP2/TP2.py
+++ TP2/TP2.py
@@ -1,11 +1,46 @@
+import sys
+import getopt
 import requests
 from bs4 import BeautifulSoup
 
-page = requests.get("https://www.imsdb.com/scripts/Titanic.html")
-page_text = page.text
+opts, args = getopt.getopt(sys.argv[1:], 'bto')
+ops = dict(opts)
 
-soup = BeautifulSoup(page_text, "html.parser")
+def build():
+    alphabet = "0ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    names_pages = []
 
-# EXTRAÇÃO APENAS DA ZONA QUE CONTEM OS NOMES DE PERSONAGENS E AS RESPETIVAS FALAS
-for child in soup.pre.children:
-    print(child)
+    #reunião de todas as páginas de nomes de filmes
+    for i in range(0,27):
+        print("Getting " + "https://www.imsdb.com/alphabetical/" + alphabet[i])
+        request = requests.get("https://www.imsdb.com/alphabetical/" + alphabet[i])
+        names_pages.append(request)
+    res = ""
+    for page in names_pages:
+        html = page.text
+        page_soup = BeautifulSoup(html, "html.parser")
+        results = page_soup.find_all('a')
+        aux = []
+        for result in results :
+            if len(result.attrs) == 2 and result.text != "":
+                aux.append(result.text)
+        aux = aux[:-5]
+        for name in aux:
+            name = name.replace(' ', '-')
+            name = name.replace(':', '')
+            res = res + 'https://www.imsdb.com/scripts/' + name + '.html' + '\n'
+    
+    f = open("films_names", "w")
+    f.write(res)
+
+# page = requests.get("https://www.imsdb.com/scripts/Titanic.html")
+# page_text = page.text
+
+# soup = BeautifulSoup(page_text, "html.parser")
+
+# # EXTRAÇÃO APENAS DA ZONA QUE CONTEM OS NOMES DE PERSONAGENS E AS RESPETIVAS FALAS
+# for child in soup.pre.children:
+#     print(child)
+
+if '-b' in ops:
+    build()
