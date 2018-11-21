1dd9bc6227fd79f6992a1d24fec34939f8c32bc1
Marco Silva <marant_silva@hotmail.com>
2018-11-21 11:15:51 +0000

    esqueci-me de tirar o comentário da extração do texto

--- TP2/TP2.py
+++ TP2/TP2.py
@@ -33,14 +33,14 @@ def build():
     f = open("films_names", "w")
     f.write(res)
 
-# page = requests.get("https://www.imsdb.com/scripts/Titanic.html")
-# page_text = page.text
+page = requests.get("https://www.imsdb.com/scripts/Titanic.html")
+page_text = page.text
 
-# soup = BeautifulSoup(page_text, "html.parser")
+soup = BeautifulSoup(page_text, "html.parser")
 
-# # EXTRAÇÃO APENAS DA ZONA QUE CONTEM OS NOMES DE PERSONAGENS E AS RESPETIVAS FALAS
-# for child in soup.pre.children:
-#     print(child)
+# EXTRAÇÃO APENAS DA ZONA QUE CONTEM OS NOMES DE PERSONAGENS E AS RESPETIVAS FALAS
+for child in soup.pre.children:
+    print(child)
 
 if '-b' in ops:
     build()
