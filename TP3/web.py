d5762d78c9ce00f24baf8494e0f497738f63384b
Marco Silva <marant_silva@hotmail.com>
2019-01-30 12:28:33 +0000

    added list of films in index.html and added new way of accessing info of movies

--- TP3/web.py
+++ TP3/web.py
@@ -1,27 +1,25 @@
 from flask import Flask, render_template, url_for, request
 from imdb import IMDb
-from tf_idf import match
+from tf_idf import match, orderDict
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 IMDb_access = IMDb()
 
 @app.route('/')
 def search():
-   return render_template('index.html')
+   return render_template('index.html', movies = orderDict)
 
-@app.route('/', methods=['POST'])
-def getMovie():
-    movie_name = request.form['movie_name']
-
+@app.route('/movie/<id>')
+def movie(id):
+    movie_name = id
     html_info = {}
 
     movies = IMDb_access.search_movie(movie_name)
 
     if movies != []:
         movie_infos = IMDb_access.get_movie(movies[0].getID())
-        print(movie_infos)
         
         directors_names = []
         try:
@@ -73,6 +71,72 @@ def getMovie():
             suggestion_error = False
         except:
             suggestion_error = True
+            suggestions = ''
+
+    return render_template('movie.html', movie_name = movie_name, html_infos = html_info, cover_url = cover_url, suggestions = suggestions, suggestion_error = suggestion_error)
+
+
+@app.route('/', methods=['POST'])
+def getMovie():
+    movie_name = request.form['movie_name']
+    html_info = {}
+
+    movies = IMDb_access.search_movie(movie_name)
+
+    if movies != []:
+        movie_infos = IMDb_access.get_movie(movies[0].getID())
+        
+        directors_names = []
+        try:
+            directors = movie_infos['directors']
+            for director in directors:
+                directors_names.append(director['name'])
+        except:
+            directors_names = []
+
+        html_info['Directors'] = ', '.join(directors_names)
+
+        try:
+            html_info['Languages'] = ', '.join(movie_infos['languages'])
+        except:
+            html_info['Languages'] = ''
+
+        try:
+            html_info['Year'] = movie_infos['year']
+        except:
+            html_info['Year'] = ''
+
+        try:
+            html_info['Kind'] = movie_infos['kind']
+        except:
+            html_info['Kind'] = ''
+
+        try:
+            html_info['Rating'] = movie_infos['rating']
+        except:
+            html_info['Rating'] = ''
+
+        try:
+            html_info['Genres'] = ', '.join(movie_infos['genres'])
+        except:
+            html_info['Genres'] = ''
+
+        try:
+            movie_name = movie_infos['title']
+        except:
+            movie_name = 'Movie not Available'
+
+        try:
+            cover_url = movie_infos['full-size cover url']
+        except:
+            cover_url = ''
+        
+        try:
+            suggestions = match(movie_infos['title'].lower())
+            suggestion_error = False
+        except:
+            suggestion_error = True
+            suggestions = ''
 
     return render_template('movie.html', movie_name = movie_name, html_infos = html_info, cover_url = cover_url, suggestions = suggestions, suggestion_error = suggestion_error)
 
